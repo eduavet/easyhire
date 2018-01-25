bab900a94123b578ab3cb704b30fe6ae3cad3eda
Lusine <lus21484@gmail.com>
2018-01-25 12:22:00 +0400

    react router example

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './react/components/App.jsx';
 import './assets/styles/stylus/index.styl';
 import { Provider } from 'react-redux';
+import { Route, Switch, BrowserRouter} from 'react-router-dom';
+
 import store from './redux/store';
 
 ReactDOM.render(
--- frontend/src/react/components/App.jsx
+++ frontend/src/react/components/App.jsx
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link,Route, Switch, BrowserRouter} from 'react-router-dom';
+
 // import { example } from '../appRedux/reducers/exampleReducer';
 
 const url = 'http://localhost:3000/api/todos/';
 
+/* Home component */
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+/* Category component */
+const Category = () => (
+    <div>
+        <h2>Category</h2>
+    </div>
+);
+
+/* Products component */
+const Products = () => (
+    <div>
+        <h2>Products</h2>
+    </div>
+);
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hello World</h1>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <h1>Hello World</h1>
+                    <div>
+                        <nav className="navbar navbar-light">
+                            <ul className="nav navbar-nav">
+
+                                /* Link components are used for linking to other views */
+                                <li><Link to="/">Homes</Link></li>
+                                <li><Link to="/category">Category</Link></li>
+                                <li><Link to="/products">Products</Link></li>
+
+                            </ul>
+                        </nav>
+                        /* Route components are rendered if the path prop matches the current URL */
+                        <Route path="/" component={Home}/>
+                        <Route path="/category" component={Category}/>
+                        <Route path="/products" component={Products}/>
+                    </div>
+                </div>
+            </BrowserRouter>
         );
     }
 }
