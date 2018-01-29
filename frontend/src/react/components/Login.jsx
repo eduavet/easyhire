95211702fe0d9f4114612752cff929530762c25c
Naira Petrosyan <nairapetrosya@gmail.com>
2018-01-29 18:42:58 +0400

    stylus bug fixed

---
+++ frontend/src/react/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link, Route, Switch, BrowserRouter} from 'react-router-dom';
+
+
+export  default class Login extends Component{
+    render(){
+        return (<div className="login-background">
+                <a className="btn loginBtn loginBtn--google" href="http://localhost:3000/auth/google">Login with Google</a>
+            </div>
+        )
+    }
+}
+
+
+
