bbdb5ef67d8addbef74b921a121e00f8727e99ff
eduavet <eduardavetisyan@gmail.com>
2018-01-29 12:59:55 +0400

    cleanup, refactoring, env variable

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -4,9 +4,6 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const util =  require('util');
 const emailsModel = require('../models/emailsModel.js');
 
-const GOOGLE_CLIENT_ID      = "483858413857-ku2lhjms1a70a0jqoocpqcucbon9ms4c.apps.googleusercontent.com"
-    , GOOGLE_CLIENT_SECRET  = "PV9sFq6lH2ZzfQKemaWM83Te";
-
 passport.serializeUser(function(user, done) {
     done(null, user);
 });
@@ -16,8 +13,8 @@ passport.deserializeUser(function(obj, done) {
 });
 
 passport.use(new GoogleStrategy({
-        clientID:     GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
+        clientID:     process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         //NOTE :
         //Carefull ! and avoid usage of Private IP, otherwise you will get the device_id device_name issue for Private IP during authentication
         //The workaround is to set up thru the google cloud console a fully qualified domain name such as http://mydomain:3000/
@@ -29,8 +26,6 @@ passport.use(new GoogleStrategy({
     },
     function (request, accessToken, refreshToken, profile, done) {
      /* Emails --*/
-     console.log('function is working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-     console.log(accessToken, 'accessToken function is working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
      request.session.userID = profile.id;
      request.session.accessToken = accessToken;
      request.session.name = profile.displayName;
@@ -45,57 +40,7 @@ passport.use(new GoogleStrategy({
         // console.log(profile.id, 'id');// 101815959642399011142 id
         // console.log(profile, 'profile');
         // const userId = profile.id;
-        // fetch('https://www.googleapis.com/gmail/v1/users/'+profile.id+'/messages?access_token='+accessToken)
-        //     .then(res => res.json()).then(res => {
-        //     const id = res.messages[1].id;
-        //     return fetch('https://www.googleapis.com/gmail/v1/users/'+profile.id+'/messages/'+id+'?access_token='+accessToken)
-        //         .then(res=> res.json())
-        //         .then(res => {
-        //             console.log('email');
-        //             emailsModel.find({"userId":userId})
-        //                 .then(res => {
-        //                     if(res.length<1) {
-        //                         console.log('user not found');
-        //                         const newEmail = new emailsModel({
-        //                             userId: userId,
-        //                             allEmails : [
-        //                                 {
-        //                                     status: 'Not reviewed',
-        //                                     emails: [{ emailId: id, isRead: false }]
-        //                                 }
-        //                             ]
-        //                         });
-        //                         newEmail.save();
-        //                     } else {
-        //                         console.log('user found');
-        //                         return emailsModel.find({userId:userId, "allEmails.emails.emailId": id})
-        //                             .then(res => {
-        //                                 console.log(' after email search');
-        //                                 console.log(' id', id);
-        //                                 if(res.length<1) {
-        //                                     console.log('email not found');
-        //                                     return emailsModel.update({userId:userId, "allEmails.status": "Not reviewed"},{ $push: { "allEmails.$.emails": { emailId: id, isRead: false }}})
-        //                                         .then(res => {
-        //                                             console.log(res);
-        //                                         })
-        //                                 } else {
-        //                                     console.log('email found')
-        //                                 }
-        //                                 console.log(res);
-        //                             })
-        //                     }
-        //                     console.log(res);
-        //                 })
-        //             // console.log(util.inspect(res, { depth: 8 }));
-        //             // console.log(res)
-        //             // // console.log(res.payload.parts[0].parts, 'payload parts')
-        //             // console.log(res.payload.parts, 'payload parts')
-        //             // console.log(Buffer.from(res.payload.parts[0].body.data, 'base64').toString()) //actual email text
-        //         })
-        // }).catch(err => {
-        //     console.log('err')
-        //     console.log(err, 'err')
-        // });
+
         // // asynchronous verification, for effect...
         // process.nextTick(function () {
         //
--- backend/handlers/handlers.js
+++ backend/handlers/handlers.js
@@ -1,5 +1,3 @@
-// const db = require('../db.js')
-// const mongoose = require('mongoose');
 const util = require('util');
 const fetch = require('node-fetch');
 const moment = require('moment');
@@ -29,10 +27,9 @@ Handlers.apiAddUser = (req, res) => {
 
 Handlers.getEmails = (req, response) => {
 
-  // console.log(req.session);
   const userId = req.session.userID;
   const accessToken = req.session.accessToken;
-  const emailsOnPage = 3;
+  const emailsOnPage = 5;
   const name = req.session.name;
   const emailsToSend = []
 
@@ -41,66 +38,32 @@ Handlers.getEmails = (req, response) => {
     .then(res => {
       const messages = res.messages
       const promises = [];
-        console.log(accessToken, 'accessToken111111111111111111111111111111111111111111111')
-        console.log(res, 'res2222222222222222222222222222222222222222222222222222')
-        console.log(req.session, 'req.session3333333333333333333333333')
-
 
       for(let i = 0; i < emailsOnPage; i++) {
         const id = messages[i].id;
-          promises.push(fetch('https://www.googleapis.com/gmail/v1/users/' + userId + '/messages/' + id + '?access_token=' + accessToken)
-            .then(res=> res.json())
-            .then(res => {
-              // console.log(util.inspect(res, { depth: 8 }));
-              const emailID = res.id;
-              const snippet = res.snippet;
-              const date = moment(new Date(res.internalDate * 1000)).format('MMMM Do YYYY, h:mm:ss a');
-              // const date = new Date(res.internalDate * 1000);
-              const subject = res.payload.headers.filter((item) => {
-                return item.name == 'Subject'
-              })[0].value
-              const sender = res.payload.headers.filter((item) => {
-                return item.name == 'From'
-              })[0].value
-              // const subject = res.payload.headers[20].value;
-              // const sender = res.payload.headers[17].value
-              const data = {emailID, sender, subject, snippet, date};
-              emailsToSend.push(data)
-                emailsModel.find({"userId":userId})
+        promises.push(fetch('https://www.googleapis.com/gmail/v1/users/' + userId + '/messages/' + id + '?access_token=' + accessToken)
+          .then(res => res.json())
+          .then(res => {
+            emailsToSend[i] = extractEmailData(res);
+            emailsModel.find({"userId": userId})
+              .then(res => {
+                if(res.length < 1) {
+                  const newEmail = buildNewEmailModel(userId, id);
+                  newEmail.save();
+                } else {
+                  emailsModel.find({userId:userId, "allEmails.emails.emailId": id})
                     .then(res => {
-                        if(res.length<1) {
-                            console.log('user not found');
-                            const newEmail = new emailsModel({
-                                userId: userId,
-                                allEmails : [
-                                    {
-                                        status: 'Not reviewed',
-                                        emails: [{ emailId: id, isRead: false }]
-                                    }
-                                ]
-                            });
-                            newEmail.save();
-                        } else {
-                            console.log('user found');
-                            emailsModel.find({userId:userId, "allEmails.emails.emailId": id})
-                                .then(res => {
-                                    if(res.length<1) {
-                                        console.log('email not found');
-                                        emailsModel.update({userId:userId, "allEmails.status": "Not reviewed"},{ $push: { "allEmails.$.emails": { emailId: id, isRead: false }}})
-                                    } else {
-                                        console.log('email found')
-                                    }
-                                })
-                        }
+                      if(res.length < 1) {
+                        emailsModel.update({userId:userId, "allEmails.status": "Not reviewed"},{ $push: { "allEmails.$.emails": { emailId: id, isRead: false }}})
+                      }
                     })
-                    console.log('yo');
-              }))
-
-        }
+                }
+              })
+          }))
+      }
 
       return Promise.all(promises)
         .then(() => {
-          // console.log(res);
           const packed = {
             name,
             emailsToSend
@@ -111,9 +74,37 @@ Handlers.getEmails = (req, response) => {
     .catch(console.error);
 }
 
+const decodeHtmlEntity = (str) => {
+  return str.replace(/&#(\d+);/g, function(match, dec) {
+    return String.fromCharCode(dec);
+  });
+};
+
+const extractEmailData = (res) => {
+  const emailID = res.id;
+  const sender = res.payload.headers.filter((item) => {
+    return item.name == 'From'
+  })[0].value
+  const subject = res.payload.headers.filter((item) => {
+    return item.name == 'Subject'
+  })[0].value
+  const snippet = decodeHtmlEntity(res.snippet);
+  const date = moment.unix(res.internalDate / 1000).format('MMMM Do YYYY, h:mm:ss a');
+  return {emailID, sender, subject, snippet, date};
+}
+
+const buildNewEmailModel = (userId, id) => {
+  return new emailsModel({
+    userId: userId,
+    allEmails : [
+      {
+        status: 'Not reviewed',
+        emails: [{ emailId: id, isRead: false }]
+      }
+    ]
+  });
+}
 
 // console.log(util.inspect(res, { depth: 8 }));
-// console.log(res)
-// // console.log(res.payload.parts[0].parts, 'payload parts')
 // console.log(res.payload.parts, 'payload parts')
 // console.log(Buffer.from(res.payload.parts[0].body.data, 'base64').toString()) //actual email text
