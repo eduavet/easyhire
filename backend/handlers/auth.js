f476bc4049f01386039c15630d0e1c5bc735f4d7
eduavet <eduardavetisyan@gmail.com>
2018-02-02 13:00:29 +0400

    added eslint

--- backend/handlers/auth.js
+++ backend/handlers/auth.js
@@ -1,42 +1,39 @@
-const util = require('util');
-const mongoose = require('mongoose');
-const fetch = require('node-fetch');
-const moment = require('moment');
-const emailsModel = require('../models/emailsModel.js');
-const usersModel = require('../models/usersModel.js');
-const foldersModel = require('../models/foldersModel.js');
+const UsersModel = require('../models/usersModel.js');
+
 const authHandlers = {};
 
 module.exports = authHandlers;
-ObjectId = require('mongodb').ObjectID;
+// ObjectId = require('mongodb').ObjectID;
 
+// function not used right now
 authHandlers.apiAddUser = (req, res) => {
-    //if user doesn't exist in DB, add user
-    usersModel.findOne({googleUser_id: req.body.googleID}, (err, user) => {
-        if(!user) return;
-        const newUser = new usersModel({
-          googleUser_id: req.body.googleID,
-          name: req.body.name,
-          image: req.body.imageURL,
-          email: req.body.email,
-        });
-        newUser.save().then(user => {
-          console.log('user added in DB');
-        })
-        .catch(console.error);
+  // if user doesn't exist in DB, add user
+  UsersModel.findOne({ googleUser_id: req.body.googleID }, (err, user) => {
+    if (!user) return;
+    const newUser = new UsersModel({
+      googleUser_id: req.body.googleID,
+      name: req.body.name,
+      image: req.body.imageURL,
+      email: req.body.email,
+    });
+    newUser.save().then((savedUser) => {
+      // respond something
+      console.log('user added in DB', savedUser);
     })
+      .catch(console.error);
+  });
 };
 
 // returns logged in user username
 authHandlers.username = (req, res) => {
-    if(req.session.name) {
-        res.json({name : req.session.name});
-    } else {
-        res.json({name : ''});
-    }
+  if (req.session.name) {
+    res.json({ name: req.session.name });
+  } else {
+    res.json({ name: '' });
+  }
 };
 
 authHandlers.logout = (req, res) => {
-    ["userID", "accessToken", "name", "passport"].forEach(e => delete req.session[e]);
-    res.redirect("http://localhost:8080");
+  ['userID', 'accessToken', 'name', 'passport'].forEach(e => delete req.session[e]);
+  res.redirect('http://localhost:8080');
 };
