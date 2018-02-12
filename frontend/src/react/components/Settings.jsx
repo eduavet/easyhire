095a45830527e14b971c927a6571fb20f5981bbb
eduavet <eduardavetisyan@gmail.com>
2018-02-12 13:12:07 +0400

    add settings dropdown and links

--- frontend/src/react/components/Settings.jsx
+++ frontend/src/react/components/Settings.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Statuses from './Statuses.jsx';
+import Templates from './Templates.jsx';
+import Signature from './Signature.jsx';
+
 export default function Settings() {
   return (
-    <div />
+    <div>
+      <Route path="/settings/statuses" component={Statuses} />
+      <Route path="/settings/templates" component={Templates} />
+      <Route path="/settings/signature" component={Signature} />
+    </div>
   );
 }
